450bd2f53f09811f571b20c2f067d9ba12733ee0
Denny Juwono <denny@mx51.io>
2020-11-01 20:31:40 +1100

    progress

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'product-list',
+    loadChildren: () => import('../app/modules/product-list/product-list.module').then(m => m.ProductListModule)
+  },
+  {
+    path: 'shopping-list',
+    loadChildren: () => import('../app/modules/shopping-list/shopping-list.module').then(m => m.ShoppingListModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
